e2d5565aac168ee1dc85051132ab88dae02a7ba0
Thanakrit <thanakrit_la@cmu.ac.th>
2026-01-06 15:11:18 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,39 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double dataset[], int array_size, double result_statistics[]){
+    double sum_of_data = 0.0;
+    double sum_of_square_data = 0.0;
+    double product_of_data = 1.0;
+    double sum_of_reciprocal_data = 0.0;
+    
+    double maximum_data = dataset[0];
+    double minimum_data = dataset[0];
+    
+    for(int i = 0; i < array_size; i++){
+        double piece_of_data = dataset[i];
+        sum_of_data += piece_of_data;
+        sum_of_square_data += piece_of_data * piece_of_data;
+        product_of_data *= piece_of_data;
+        sum_of_reciprocal_data += 1.0 / piece_of_data;
+        if(piece_of_data > maximum_data){
+            maximum_data = piece_of_data;
+        }
+        if(piece_of_data < minimum_data){
+            minimum_data = piece_of_data;
+        }
+    }
+    double arithmetic_mean = sum_of_data / array_size;
+    double variance = (sum_of_square_data / array_size) - (arithmetic_mean * arithmetic_mean);
+    double standard_deviation = sqrt(variance);
+    double geometric_mean = pow(product_of_data, 1.0 / array_size);
+    double harmonic_mean = array_size / sum_of_reciprocal_data;
+    
+    result_statistics[0] = arithmetic_mean;
+    result_statistics[1] = standard_deviation;
+    result_statistics[2] = geometric_mean;
+    result_statistics[3] = harmonic_mean;
+    result_statistics[4] = maximum_data;
+    result_statistics[5] = minimum_data;
+}
